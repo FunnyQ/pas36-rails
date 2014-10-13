c8e8a7d0d527301d099d4831ffd716428e9fae89
FunnyQ <funnyq@funnyq.com>
2014-10-13 13:58:10 +0800

    done news cover uploader

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,3 +1,6 @@
 class News < ActiveRecord::Base
   include Bootsy::Container
+  mount_uploader :img, NewscoverUploader
+
+  validates_presence_of :title, :img, :content
 end
--- app/uploaders/newscover_uploader.rb
+++ app/uploaders/newscover_uploader.rb
@@ -1,10 +1,10 @@
 # encoding: utf-8
 
-class CoverUploader < CarrierWave::Uploader::Base
+class NewscoverUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -13,7 +13,7 @@ class CoverUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/newscover/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -25,7 +25,7 @@ class CoverUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+  process :resize_to_fill => [200, 200]
   #
   # def scale(width, height)
   #   # do something
@@ -38,9 +38,9 @@ class CoverUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
